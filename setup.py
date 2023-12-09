e376b81bac3f309fb2d9ea7f65a4f745f911e384
Ahmed Nafies <ahmed.nafies@gmail.com>
2023-12-09 12:42:48 +0100

    fix: moves to python 3.9

--- setup.py
+++ setup.py
@@ -33,10 +33,9 @@ setup(
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     zip_safe=False,
-    python_requires=">=3.6",
+    python_requires=">=3.9",
 )
